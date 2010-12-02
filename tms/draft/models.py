c0b68db815b16d08bdef316d4539d607e2f1d974
Tomasz Wsuł <2nickers@gmail.com>
2010-12-02 16:21:00 +0100

    propozycja modeli

--- tms/draft/models.py
+++ tms/draft/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+#
+# project = {id:int, parent_project:int, name:string, slug:slug, desc:text, budget:int, start_date:timestamp, planned:work_time}
+# note = {id:int, name:string, desc:text, files:[], project_id:int, add_date:timestamp}
+# work_time = {id:int, project_id:int, start_time:timestamp, end_time:timestamp, comment:text}
+# event:work_time = {}
+#
