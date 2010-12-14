cf10e91bce8915162cfd4c48aa0b8ba394722162
nickers <2nickers@gmail.com>
2010-12-14 23:50:28 +0100

    pełne środowisko zastąpione listą pakietów do zainstalowania przez 'pip install -r packages.txt'

--- .virtualenv/bin/django-admin.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python2.6
-import os; activate_this=os.path.join(os.path.dirname(__file__), 'activate_this.py'); execfile(activate_this, dict(__file__=activate_this)); del os, activate_this
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
