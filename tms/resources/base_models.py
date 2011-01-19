0024f9e9a2e7d40aa6c40f759296718236b79663
Tomasz Wsuł <2nickers@gmail.com>
2011-01-19 22:58:19 +0100

    zaczątek klasy

---
+++ tms/resources/base_models.py
@@ -0,0 +1,61 @@
+from piston.handler import BaseHandler
+from piston.utils import rc
+
+
+# handlers
+class AuthBaseHandler(BaseHandler):
+    allowed_methods = ('GET', 'DELETE', 'PUT', 'POST')
+    model = Project
+    model_filter = AuthProject
+    fields = ('name', 'slug', 'start_date', 'parent_project', 'planned_work', 'budget', 'description', ('user',('id','username')))
+
+    def pre_create(self, request, *args, **kwargs):
+        """ Empty pre-create action. Overrite to modify data before create is called. """
+        None
+        
+    def pre_read(self, request, id):
+        None
+        
+    def post_read(self, request, id, results):
+        return results
+
+
+    def create(self, request, *args, **kwargs):
+        self.pre_create(request, *args, **kwargs)
+        return super(self).create(self, request, *args, **kwargs)
+    
+    def read(self, request, id=None):
+        """
+        Returns object with given id ("404 Not found" if no object with given id)
+        or list of objects. Both are filtered only for logged user.
+        """
+        base = self.model_filter(request.user).get()
+        
+        self.pre_read(request, id)
+        
+        if id:
+            try:
+                return self.post_read(request, id, base.get(pk=id))
+            except:
+                return rc.NOT_FOUND
+        else:
+            return self.post_read(request, id, base.all())
+
+    def delete(self, request, id):
+        base = self.model_filter(request.user).get()
+        base.filter(pk=id).delete()
+        return rc.DELETED
+
+    def update(self, request, id):
+        base = self.model_filter(request.user).get()
+        data = request.POST
+        for fld in self.model_filter.exclude:
+            del data[fld]
+
+        obj = None
+        try:
+            obj = base.get(id)
+        except:
+            obj = self.model()
+        
+        fields = [x[0].name for x in obj._meta.get_fields_with_model()]
--- tms/resources/models.py
+++ tms/resources/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from piston.handler import BaseHandler
 from piston.utils import rc
 from draft.models import *
+from base_models import AuthBaseHandler
 
 # filters
 class AuthProject():
@@ -20,55 +21,17 @@ class AuthNote():
         return self.objects
 
 # handlers
-class AuthProjectHandler(BaseHandler):
+class AuthProjectHandler(AuthBaseHandler):
     allowed_methods = ('GET', 'DELETE', 'PUT', 'POST')
     model = Project
     model_filter = AuthProject
     fields = ('name', 'slug', 'start_date', 'parent_project', 'planned_work', 'budget', 'description', ('user',('id','username')))
     
-    def create(self, request, *args, **kwargs):
+    def pre_create(self, request, *args, **kwargs):
         request.POST['user'] = request.user.id
-        return BaseHandler.create(self, request, *args, **kwargs)
-    
-    def read(self, request, id=None):
-        """
-        Returns object with given id ("404 Not found" if no object with given id)
-        or list of objects. Both are filtered only for logged user.
-        """
-        base = self.model_filter(request.user).get()
-        
-        if id:
-            try:
-                return base.get(pk=id)
-            except:
-                return rc.NOT_FOUND
-        else:
-            return base.all()
-
-    def delete(self, request, id):
-        base = self.model_filter(request.user).get()
-        base.filter(pk=id).delete()
-        return rc.DELETED
-
-    def update(self, request, id):
-        base = self.model_filter(request.user).get()
-        data = request.POST
-        for fld in self.model_filter.exclude:
-            del data[fld]
-
-        obj = None
-        try:
-            obj = base.get(id)
-        except:
-            obj = self.model()
-        
-        fields = [x[0].name for x in obj._meta.get_fields_with_model()]
-        
-        
-            
         
 
-class AuthNoteHandler(AuthProjectHandler):
+class AuthNoteHandler(AuthBaseHandler):
     model = Note
     model_filter = AuthNote
     fields = ('id', 'name', 'description', ('files', ('id','file')), ('project', ('id', 'name')), 'add_time')
