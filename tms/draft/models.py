8fe255215dd118cf4416cda87b493a367dcf9cf0
Tomasz Wsuł <2nickers@gmail.com>
2011-01-25 18:10:31 +0100

    pliki już nie są wymagane w notatkach

--- tms/draft/models.py
+++ tms/draft/models.py
@@ -37,7 +37,7 @@ class UploadedFile(models.Model):
 class Note(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField(blank=True)
-    files = models.ManyToManyField(UploadedFile, null=False)
+    files = models.ManyToManyField(UploadedFile, null=True, blank=True)
     project = models.ForeignKey(Project)
     add_time = models.DateTimeField(auto_now_add=True)
 
